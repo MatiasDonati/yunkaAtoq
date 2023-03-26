1def4defeb6c885aba047107a5325354d7ab8a63
MatiasDonati <matod87@hotmail.com>
2023-03-26 16:07:21 -0300

    YunkaLogo en Navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,15 +1,14 @@
 import { BsWhatsapp, BsInstagram } from "react-icons/bs";
 import { AiOutlineFacebook } from "react-icons/ai";
+import YunkaLogo from "./assets/YunkaLogo.png";
 
 const NavBar = () => {
   return (
-    <div
-    className=
-    "fixed w-full h-[100px] items-center"
-    >
+    <div className="fixed w-full h-[100px] items-center">
 
-    {/* Navbar 1era Secciom */}
+      {/* Navbar 1era Secciom */}
       <div className="bg-[#0E385B] w-full h-[100px] flex justify-between items-center text-3xl font-bold">
+
         <div className=" pl-10">
           <div className="text-white">YUNKA ATOQ</div>
           <div className="text-zinc-300	text-sm font-light italic pl-1">
@@ -17,6 +16,11 @@ const NavBar = () => {
             <div>Cochabamba, Bolivia</div>
           </div>
         </div>
+
+        <div>
+          <img src={YunkaLogo} className="w-[180px] pt-40" />
+        </div>
+
         <div className="text-white">
           <ul className="flex pr-10">
             <li>
@@ -30,15 +34,15 @@ const NavBar = () => {
             </li>
           </ul>
         </div>
+
       </div>
 
       {/* Navbar 2da Seccion */}
-
-      <div className='bg-[#0E385B] w-full h-[50px] items-center border-y-4'>
-        <ul className='flex justify-between text-white text-lg px-10 pt-2'>
+      <div className="bg-[#0E385B] w-full h-[50px] items-center border-y-4">
+        <ul className="flex justify-between text-white text-lg px-10 pt-2">
           <li>Nosotros</li>
           <li>Donaciones</li>
-          <li>** LogoZorro **</li>
+          {/* <li><img src={YunkaLogo} className='w-[180px]'/></li> */}
           <li>Capacitaciones</li>
           <li>Incorpórate</li>
         </ul>
