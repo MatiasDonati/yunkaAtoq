f9bb1ac0aa5ce0875c3ec453e5b7b0a9d1334402
MatiasDonati <matod87@hotmail.com>
2023-03-28 23:36:08 -0300

    hovers navbar y logo, Formulario

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -5,11 +5,9 @@ import YunkaLogo from "./assets/YunkaLogo.png";
 const NavBar = () => {
   return (
     <div className="fixed w-full h-[100px] items-center">
-
       {/* Navbar 1era Secciom */}
       <div className="bg-[#0E385B] w-full h-[100px] flex justify-between items-center text-3xl font-bold">
-
-        <div className=" pl-10">
+        <div className="pl-10 flex-1">
           <div className="text-white">YUNKA ATOQ</div>
           <div className="text-zinc-300	text-sm font-light italic pl-1">
             <div>Bomberos Volunarios</div>
@@ -17,37 +15,59 @@ const NavBar = () => {
           </div>
         </div>
 
-        <div>
-          <img src={YunkaLogo} className="w-[180px] pt-40" />
+        <div className=" flex flex-1 justify-center">
+          <img
+            src={YunkaLogo}
+            className="w-[180px] pt-40 transition-all duration-400 ease-in-out hover:w-[220px]"
+          />
         </div>
 
-        <div className="text-white">
-          <ul className="flex pr-10">
+        <div className="flex-1 text-white">
+          <ul className="flex pr-10 justify-end">
             <li>
-              <BsWhatsapp />
+              <BsWhatsapp className="hover:text-[40px] transition-all duration-400 ease-in-out" />
             </li>
-            <li>
+            {/* <li className="hover:text-[40px]">
               <BsInstagram />
-            </li>
+            </li> */}
             <li>
-              <AiOutlineFacebook />
+              <AiOutlineFacebook className="hover:text-[40px] transition-all duration-400 ease-in-out" />
             </li>
           </ul>
         </div>
-
       </div>
 
       {/* Navbar 2da Seccion */}
-      <div className="bg-[#0E385B] w-full h-[50px] items-center border-y-4">
-        <ul className="flex justify-between text-white text-lg px-10 pt-2">
-          <li>Nosotros</li>
-          <li>Donaciones</li>
-          {/* <li><img src={YunkaLogo} className='w-[180px]'/></li> */}
-          <li>Capacitaciones</li>
-          <li>Incorpórate</li>
-        </ul>
+      {/* Primera opcion */}
+      <div className="flex w-full flex-row bg-[#0E385B] h-[50px] items-center border-y-4 text-white text-lg">
+        <div className="flex-1 px-20">
+          <ul className="flex justify-between">
+            <li className="flex-1 text-center hover:text-[20px] transition-all duration-400 ease-in-out">Nosotros</li>
+            <li className="flex-1 text-center hover:text-[20px] transition-all duration-400 ease-in-out">Especialidades</li>
+            <li className="flex-1 text-center hover:text-[20px] transition-all duration-400 ease-in-out">Donaciones</li>
+          </ul>
+        </div>
+
+        <div className="flex-1 px-20 ml-[150px]">
+          <ul className="flex justify-between">
+            <li className="flex-1 text-center hover:text-[20px] transition-all duration-400 ease-in-out">Capacitaciones</li>
+            <li className="flex-1 text-center hover:text-[20px] transition-all duration-400 ease-in-out">Incorpórate</li>
+            <li className="flex-1 text-center hover:text-[20px] transition-all duration-400 ease-in-out">Contacto</li>
+          </ul>
+        </div>
       </div>
 
+      {/* Segunda opcion */}
+      {/* <div className="bg-[#0E385B] w-full h-[50px] items-center border-y-4">
+        <ul className="flex justify-between text-white text-lg px-10 pt-2 ">
+          <li>Nosotros</li>
+          <li>Especialidades</li>
+          <li>Donaciones</li>
+          <li>Capacitaciones</li>
+          <li>Incorpórate</li>
+          <li>Contacto</li>
+        </ul>
+      </div> */}
     </div>
   );
 };
