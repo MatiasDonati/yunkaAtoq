f7736a962020c1289197749cb8febabfc8726ccf
Gaston964 <gastongutierrez964@gmail.com>
2023-03-30 12:47:42 -0300

    Maps

--- src/components/GoogleMaps.jsx
+++ src/components/GoogleMaps.jsx
@@ -1,7 +1,9 @@
 const GoogleMaps = () => {
   return (
     <div className="bg-[#D9D9D9] h-[400px] mx-[300px] mb-8 ">
-      <div className="text-center pt-[200px]">Google Maps</div>
+      <div className="text-center pt-[200px]">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d121835.82370770606!2d-66.23391608494502!3d-17.394046887492443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93e373e0d9e4ab27%3A0xa2719ae9532c3e65!2sCochabamba%2C%20Bolivia!5e0!3m2!1ses!2sar!4v1680191138873!5m2!1ses!2sar" width="400" height="300" ></iframe>
+      </div>
     </div>
   );
 };
