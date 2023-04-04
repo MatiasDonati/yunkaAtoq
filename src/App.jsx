3c20493030e0fb016f3e37210c514880584d7991
Gaston964 <gastongutierrez964@gmail.com>
2023-04-04 00:28:11 -0300

    Router DOM e Intento de Nosotros

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import GoogleMaps from "./components/GoogleMaps"
 import Home from "./components/Home"
 import NavBar from "./components/NavBar"
 import Departamentos from "./components/Departamentos"
+import { Route, Routes } from "react-router"
+import Nosotros from "./components/Nosotros"
 
 
 
@@ -12,11 +14,10 @@ function App() {
   return (
     <>
     <NavBar />
-    <Home />
-    <Departamentos />
-    <FacebookPosts />
-    <Form />
-    <GoogleMaps />
+    <Routes>
+      <Route path="/home" element={<Home />}  />
+      <Route path="/nosotros" element={<Nosotros/>} />
+    </Routes>
     <Footer />
     </>
   )
---
+++ src/components/Nosotros.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nosotros = () => {
+    return (
+        <div className='h-[100vh]'>
+            <div>
+                <h1>| Quienes somos?</h1>
+                <p>YUNKA ATOQ es una fundación de protección medioambiental que cuenta con un brazo operativo de primera respuesta ante emergencias y desastres naturales.
+
+                    Nació en el año 2023, con el fin de abastecer las emergencias (incendios forestales, rescates, atenciones médicas) y el objetivo de capacitar a jóvenes voluntarios que de manera desinteresada y altruista, arriesguen sus vidas para salvaguardar así, las de otros seres vivos.
+
+                    Bajo el lema "Para que otros puedan vivir”, servimos al pueblo Boliviano del cual nacimos, devolviéndole a nuestra tierra un poco de todo lo que nos brindó.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Nosotros
