4163fc240e7d9297450ee55e18f7548755859eef
MatiasDonati <matod87@hotmail.com>
2023-04-13 07:09:40 -0300

    logo se achica al scrollear

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,33 +3,52 @@ import { AiOutlineFacebook, AiOutlineLinkedin } from "react-icons/ai";
 import YunkaLogo from "../assets/YunkaLogo.png";
 import { Link } from "react-scroll";
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const NavBar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  console.log(isScrolled);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 150) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="fixed w-full items-center z-10">
       {/* Navbar 1era Secciom */}
       <div className="bg-[#0E385B] w-full h-[70px] flex justify-between items-center text-3xl font-bold">
         <div className="pl-10 flex-1">
           <Link to="home" smooth={true} duration={500}>
-              <NavLink to={"/"}>
+            <NavLink to={"/"}>
               <div className="text-white cursor-pointer">YUNKA ATOQ</div>
-              </NavLink>
+            </NavLink>
           </Link>
         </div>
 
         <div className=" flex flex-1 justify-center">
           <Link to="home" smooth={true} duration={500}>
             <NavLink to={"/"}>
-            <img
-              src={YunkaLogo}
-              className={`w-[180px] pt-40 transition-all duration-400 ease-in-out hover:w-[200px] cursor-pointer`}
-            />
+              <img
+                src={YunkaLogo}
+                className={`${isScrolled ? 'w-[100px] pt-[40px]': 'w-[180px] pt-40'} transition-all duration-400 ease-in-out hover:w-[200px] cursor-pointer`}
+              />
             </NavLink>
           </Link>
         </div>
 
         <div className="flex-1 text-white pr-10">
-
           <ul className="flex justify-end">
             <li className="h-10 w-16">
               <BsTelephone className="hover:text-[40px] transition-all duration-400 ease-in-out" />
@@ -45,7 +64,6 @@ const NavBar = () => {
             </li>
           </ul>
         </div>
-       
       </div>
 
       {/* Navbar 2da Seccion */}
@@ -53,7 +71,12 @@ const NavBar = () => {
       <div className="flex w-full flex-row bg-[#0E385B] h-[50px] items-center border-y-4 text-white text-lg">
         <div className="flex-1 px-20">
           <ul className="flex justify-between">
-            <NavLink className="flex-1 text-center hover:text-[20px] transition-all duration-400 ease-in-out" to="/nosotros">Nosotros</NavLink>
+            <NavLink
+              className="flex-1 text-center hover:text-[20px] transition-all duration-400 ease-in-out"
+              to="/nosotros"
+            >
+              Nosotros
+            </NavLink>
             {/* <li className="flex-1 text-center hover:text-[20px] transition-all duration-400 ease-in-out">
               Nosotros
             </li> */}
