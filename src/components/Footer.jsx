e9b0aef4ab852393ca0df1e2a31a93d80485a005
MatiasDonati <matod87@hotmail.com>
2023-04-19 06:32:34 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
 
       {/* 1era Seccion */}
       <div className="bg-[#0E385B] w-full h-[80px] items-center text-white flex justify-center">
-        <div className="flex items-center pr-20">
+        <div className="flex items-center pr-20 mx-20">
           <div className="pr-2 text-[30px]">
             <FiMail />
           </div>
           <div className="text-[20px]">informaciones@yunkaatoq.org</div>
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center mx-20">
           <div className="pr-2 text-[30px]">
             <BsTelephone />
           </div>
