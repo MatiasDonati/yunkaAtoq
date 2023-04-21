4af02f563deb040e6dd3bc2b950fea31a5f9bb86
Gaston964 <gastongutierrez964@gmail.com>
2023-04-21 13:04:22 -0300

    carousel 2.0

--- src/components/PartesLogo.jsx
+++ src/components/PartesLogo.jsx
@@ -1,82 +1,58 @@
-import React from 'react';
-import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
-import sombrero from "../assets/parteLogos/atom/Sombrero.png";
-import engranajes from "../assets/parteLogos/atom/Engranajes.png";
-import cruzAmarilla from "../assets/parteLogos/atom/CruzAmarilla.png";
-import corazon from "../assets/parteLogos/atom/Corazon.png";
-import cruzBlanca from "../assets/parteLogos/atom/CruzBlanca.png";
-import hachas from "../assets/parteLogos/atom/Hachas.png";
+import React, { useRef, useEffect } from 'react';
+import Flickity from 'flickity';
+import 'flickity/dist/flickity.min.css';
 
-SwiperCore.use([Navigation, Pagination, Autoplay]);
+import sombrero from "../assets/parteLogos/atom/Sombrero.png"
+import engranajes from "../assets/parteLogos/atom/Engranajes.png"
+import cruzAmarilla from "../assets/parteLogos/atom/CruzAmarilla.png"
+import corazon from "../assets/parteLogos/atom/Corazon.png"
+import cruzBlanca from "../assets/parteLogos/atom/CruzBlanca.png"
+import hachas from "../assets/parteLogos/atom/Hachas.png"
 
 const PartesLogo = () => {
-  const slides = [
-    {
-      id: 1,
-      image: sombrero,
-      alt: "1"
-    },
-    {
-      id: 2,
-      image: engranajes,
-      alt: "2"
-    },
-    {
-      id: 3,
-      image: cruzAmarilla,
-      alt: "3"
-    },
-    {
-      id: 4,
-      image: corazon,
-      alt: "4"
-    },
-    {
-      id: 5,
-      image: cruzBlanca,
-      alt: "5"
-    },
-    {
-      id: 6,
-      image: hachas,
-      alt: "6"
-    },
-  ];
+  const flickityRef = useRef(null);
 
-  const swiperParams = {
-    slidesPerView: 4,
-    spaceBetween: -100,
-    navigation: true,
-    loop: false,
-    autoplay: { delay: 2000 },
-    breakpoints: {
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
-      640: {
-        slidesPerView: 1,
-        spaceBetween: 0,
-      },
-    },
-    initialSlide: 0,
-  };
+  useEffect(() => {
+    const flickityInstance = new Flickity(flickityRef.current, {
+      cellAlign: 'left',
+      wrapAround: true,
+      prevNextButtons: true,
+      pageDots: false,
+      autoPlay: 3000,
+      initialIndex: 0,
+      dragThreshold: 10,
+      selectedAttraction: 0.1,
+      friction: 0.8,
+      freeScroll: true,
+      contain: true,
+      lazyLoad: 2,
+      imagesLoaded: true,
+    });
 
+    return () => flickityInstance.destroy();
+  }, []);
 
   return (
-    <Swiper {...swiperParams} className='bg-fondo2 p-10 '>
-      {slides.map((slide) => (
-        <SwiperSlide key={slide.id}>
-          <img src={slide.image} alt={slide.alt} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div ref={flickityRef} className='carousel bg-fondo2'>
+      <div className='carousel-cell'>
+        <img src={sombrero} alt='1' />
+      </div>
+      <div className='carousel-cell'>
+        <img src={engranajes} alt='2' />
+      </div>
+      <div className='carousel-cell'>
+        <img src={cruzAmarilla} alt='3' />
+      </div>
+      <div className='carousel-cell'>
+        <img src={corazon} alt='4' />
+      </div>
+      <div className='carousel-cell'>
+        <img src={cruzBlanca} alt='5' />
+      </div>
+      <div className='carousel-cell'>
+        <img src={hachas} alt='6' />
+      </div>
+    </div>
   );
 };
 
