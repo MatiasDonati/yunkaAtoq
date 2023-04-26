b3b99c5ef134fbc6b462b5dab7dc99d3b9db5495
Gaston964 <gastongutierrez964@gmail.com>
2023-04-26 12:57:49 -0300

    Formacion

--- src/components/Formacion.jsx
+++ src/components/Formacion.jsx
@@ -24,9 +24,9 @@ const Formacion = () => {
   ];
 
   return (
-    <div className="bg-[#023763] h-[400px] text-white flex items-center px-20 mb-20">
+    <div className="bg-[#023763] lg:h-[400px] text-white flex items-center px-20 mb-20 md:flex flex-wrap px-2 py-3">
       {formacion.map((el, indice) => (
-        <div key={indice} className="flex-1 border-2 rounded-lg mr-2 mx-10 bg-[#052B4A]">
+        <div key={indice} className="flex-1 border-2 rounded-lg mr-2 mx-10 bg-[#052B4A] md: m-2">
           <img src={el.logo} alt="" className="mx-auto block p-2 mb-20 mt-10" />
           <div id="hola" className="bg-[#C28507] border-t-2 rounded h-14 flex justify-center items-center text-xl font-bold">{el.titulo}</div>
         </div>
