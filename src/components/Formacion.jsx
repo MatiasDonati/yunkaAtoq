066e67d632d18a3583c7642f1f9cabb9f999e668
MatiasDonati <matod87@hotmail.com>
2023-04-28 06:17:15 -0300

    finiquitando

--- src/components/Formacion.jsx
+++ src/components/Formacion.jsx
@@ -5,7 +5,6 @@ import Incendio from "../assets/Incendio.png";
 import { useEffect, useState } from "react";
 
 const Formacion = () => {
-
   const [windowWidth, setWindowWidth] = useState(0);
   useEffect(() => {
     function handleResize() {
@@ -15,7 +14,6 @@ const Formacion = () => {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-
 
   const formacion = [
     {
@@ -37,11 +35,29 @@ const Formacion = () => {
   ];
 
   return (
-    <div className={`${windowWidth < 470 ? 'bg-[#023763] text-white py-14' : 'bg-[#023763] lg:h-[400px] text-white flex items-center px-20 mb-20 md:flex flex-wrap py-3'} `}>
+    <div
+      className={`${
+        windowWidth < 470
+          ? "bg-[#023763] text-white py-14"
+          : "bg-[#023763] lg:h-[400px] text-white flex items-center px-20 mb-20 md:flex flex-wrap py-3"
+      } `}
+    >
       {formacion.map((el, indice) => (
-        <div key={indice} className={`${windowWidth < 470 ? 'flex-1 border-2 rounded-lg m-4 bg-[#052B4A]': 'flex-1 border-2 rounded-lg mr-2 mx-10 bg-[#052B4A] md: m-2'}`}>
+        <div
+          key={indice}
+          className={`${
+            windowWidth < 470
+              ? "flex-1 border-2 rounded-lg m-4 bg-[#052B4A]"
+              : "flex-1 border-2 rounded-lg mr-2 mx-10 bg-[#052B4A] md: m-2"
+          }`}
+        >
           <img src={el.logo} alt="" className="mx-auto block p-2 mb-20 mt-10" />
-          <div id="hola" className="bg-[#C28507] border-t-2 rounded h-14 flex justify-center items-center text-xl font-bold">{el.titulo}</div>
+          <div
+            id="hola"
+            className={`bg-[#C28507] border-t-2 h-14 flex justify-center items-center ${windowWidth}text-xl font-bold`}
+          >
+            {el.titulo}
+          </div>
         </div>
       ))}
     </div>
